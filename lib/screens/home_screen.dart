af1f3494bf35754b0670a1ff8e177da558603638
belhamra soufyane <soufi-43@hotmail.fr>
2020-07-16 16:14:44 +0100

    user profile and logout

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -25,11 +25,11 @@ class _HomeScreenState extends State<HomeScreen> with WidgetsBindingObserver {
   @override
   void initState() {
     super.initState();
-    SchedulerBinding.instance.addPostFrameCallback((_) {
+    SchedulerBinding.instance.addPostFrameCallback((_) async {
       userProvider = Provider.of<UserProvider>(context, listen: false);
 
-      userProvider.refreshUser();
-      _authMethods.setUserState(userId: userProvider.getUser.uid, userState: UserState.Online)
+      await userProvider.refreshUser();
+      _authMethods.setUserState(userId: userProvider.getUser.uid, userState: UserState.Online) ;
 
 
     });
--- lib/screens/pageviews/widgets/user_circle.dart
+++ lib/screens/pageviews/widgets/user_circle.dart
@@ -1,52 +1,57 @@
-
-
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:skypeclone/provider/user_provider.dart';
+import 'package:skypeclone/screens/widgets/user_details_container.dart';
 import 'package:skypeclone/utils/universal_variables.dart';
 import 'package:skypeclone/utils/utilities.dart';
 
-
 class UserCircle extends StatelessWidget {
-
-
   @override
   Widget build(BuildContext context) {
-    final UserProvider userProvider = Provider.of<UserProvider>(context) ;
+    final UserProvider userProvider = Provider.of<UserProvider>(context);
 
-    return Container(
-      height: 40,
-      width: 40,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(50),
-        color: UniversalVariables.separatorColor,
-      ),
-      child: Stack(
-        children: <Widget>[
-          Align(
-            alignment: Alignment.center,
-            child: Text(
-              Utils.getInitials(userProvider.getUser.name),
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                color: UniversalVariables.lightBlueColor,
-                fontSize: 13,
+    return GestureDetector(
+      onTap: () => showModalBottomSheet(
+          context: context,
+          builder: (context) => UserDetailsContainer(),
+          isScrollControlled: true ,
+          backgroundColor: UniversalVariables.blackColor),
+
+
+      child: Container(
+        height: 40,
+        width: 40,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(50),
+          color: UniversalVariables.separatorColor,
+        ),
+        child: Stack(
+          children: <Widget>[
+            Align(
+              alignment: Alignment.center,
+              child: Text(
+                Utils.getInitials(userProvider.getUser.name),
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: UniversalVariables.lightBlueColor,
+                  fontSize: 13,
+                ),
               ),
             ),
-          ),
-          Align(
-            alignment: Alignment.bottomRight,
-            child: Container(
-              height: 12,
-              width: 12,
-              decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  border: Border.all(
-                      color: UniversalVariables.blackColor, width: 2),
-                  color: UniversalVariables.onlineDotColor),
-            ),
-          )
-        ],
+            Align(
+              alignment: Alignment.bottomRight,
+              child: Container(
+                height: 12,
+                width: 12,
+                decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    border: Border.all(
+                        color: UniversalVariables.blackColor, width: 2),
+                    color: UniversalVariables.onlineDotColor),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
